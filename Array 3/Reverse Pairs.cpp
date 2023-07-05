529507376cadf52eb5f4252e1c354b941be6d567
Abhi24001 <101787857+Abhi24001@users.noreply.github.com>
2023-07-05 22:38:14 +0530

    Update Reverse Pairs.cpp

--- Array 3/Reverse Pairs.cpp
+++ Array 3/Reverse Pairs.cpp
@@ -1,15 +1,39 @@
 class Solution {
 public:
-    int reversePairs(vector<int>& nums) {
-        int n = nums.size();
-        long long reversePairsCount = 0;
-        for(int i=0; i<n-1; i++){
-            for(int j=i+1; j<n; j++){
-                if(nums[i] > 2*(long long)nums[j]){
-                    reversePairsCount++;
-                }
-            }
+void merge(vector<int>&arr,int low,int mid,int high,int &cnt){
+    int i=low,j=mid+1;
+    int r=j;
+    int c=0;
+    vector<int>v;
+     while(i<=mid){
+        while(r<=high and arr[i]>(long long)2*arr[r]){
+            c++;
+            r++;
         }
-        return reversePairsCount;
+        cnt+=c;
+        
+          while(j<=high and arr[j]<=arr[i])v.push_back(arr[j++]);
+          v.push_back(arr[i++]);
+     }
+     while(j<=high)v.push_back(arr[j++]);
+     for(int i=low;i<=high;i++)arr[i]=v[i-low];
+}
+
+   void mergesort(vector<int>&arr,int low,int high,int &cnt){
+       if(low<high){
+      int mid=low+(high-low)/2;
+      mergesort(arr,low,mid,cnt);
+      mergesort(arr,mid+1,high,cnt);
+      merge(arr,low,mid,high,cnt);
+       }
+   }
+    int reversePairs(vector<int>& arr) {
+      // merge sort -> modified merge function 
+    int cnt=0;
+    int n=arr.size();
+    mergesort(arr,0,n-1,cnt);
+    if(n<=1 or cnt==0)return 0;
+    return cnt;
+        
     }
 };
